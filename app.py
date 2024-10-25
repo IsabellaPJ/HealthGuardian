624867c98f065d0d2071d7320368022f777bf8ea
Isabella Priya P J <81914291+IsabellaPJ@users.noreply.github.com>
2024-10-25 14:15:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -331,4 +331,4 @@ def prediction():
     return jsonify({"disease": disease, "advice": advice})
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0, port=5000)
+    app.run(host='0.0.0.0', port=5000)
